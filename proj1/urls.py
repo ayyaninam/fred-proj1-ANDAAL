cc4c197dcb214d27d1635bc037799bc4a2c9957e
Ayyan Inam <apple@Apples-MacBook-Pro.local>
2024-01-11 21:36:05 +0500

    URL Global Prefix

--- proj1/urls.py
+++ proj1/urls.py
@@ -3,16 +3,19 @@ from django.urls import include, path
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+
+# New Code
+from django.shortcuts import redirect
+
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
 
     # The Django admin is not officially supported; expect breakage.
     # Nonetheless, it's often useful for debugging.
-
+    path('', lambda request: redirect('andaal/', permanent=True)),
     path('admin/', admin.site.urls),
-    path('', include('base.urls')),
-
-    path('', include(apps.get_app_config('oscar').urls[0])),
+    path('andaal/', include('base.urls')),
+    path('andaal/', include(apps.get_app_config('oscar').urls[0])),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
