c468c61d96504d037ab6c943ae5d5bcad163a169
Ayyan Inam <apple@Apples-MacBook-Pro.local>
2024-01-29 18:16:44 +0500

    requirements.txt updated

--- proj1/settings.py
+++ proj1/settings.py
@@ -274,8 +274,8 @@ EMAIL_BACKEND ="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "**************"
-EMAIL_HOST_PASSWORD = "**************"
+EMAIL_HOST_USER = "*************"
+EMAIL_HOST_PASSWORD = "*************"
 
 
 ARE_YOU_USING_ENV = False
