dc00e3e646d697f7ae716d7b410b7cd7d1745163
Ayyan Inam <apple@Apples-MacBook-Pro.local>
2024-01-30 12:14:21 +0500

    Fixed Prefix for Media and Static Files

--- proj1/settings.py
+++ proj1/settings.py
@@ -198,7 +198,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'andaal/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
@@ -209,7 +209,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-MEDIA_URL='/media/'
+MEDIA_URL='/andaal/media/'
 MEDIA_ROOT=BASE_DIR / 'media'
 
 
